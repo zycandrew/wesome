2e6861f83a831baf2ddca348f60474a450bad017
yachao.zhao <zyc_tiger@163.com>
2021-09-18 10:20:32 +0800

    Initial commit

---
+++ conf/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2021/9/18 9:55
+# @Author  : zyc
+# @File    : __init__.py.py
+# @Software: PyCharm
---
+++ www/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2021/9/18 10:06
+# @Author  : zyc
+# @File    : __init__.py.py
+# @Software: PyCharm
